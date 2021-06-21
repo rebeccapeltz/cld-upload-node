572c735c84cc8d22d62a3ff8918a0bff6b645172
Rebeccca Peltz <rebeccapeltz@gmail.com>
2021-06-21 17:03:04 +0000

    add comments on options for creds

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 
+/* 3 options for credentials */
+
+// use environment variables
+// repl.it provides a hidden env with a small form
+
+// go to shell and export cld url
+// export CLOUDINARY_URL=cloudinary://API_KEY:API_SECRET@CLOUD_NAME
+
+// const cldURL = "CLOUDINARY_URL=cloudinary://API_KEY:API_SECRET@CLOUD_NAME";
+// cloudinary.config(cldURL);
+
+// cloudinary.config({
+//  cloud_name: "xx", // add your cloud_name
+//  api_key: "xx", // add your api_key
+//  api_secret: "xx", // add your api_secret
+//  secure: true
+// });
+
+
 cloudinary.uploader.upload('./banana.jpg', {
   public_id: 'banana',
   type: 'upload',
